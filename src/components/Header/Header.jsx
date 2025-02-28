8cda46805ef1916a23acdd3f894eb758c8253254
mreza-slt <rsltany649@gmail.com>
2025-02-28 11:38:56 +0330

    feat(create header search items in header component): add header component and change any component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import { HiCalendar, HiSearch } from "react-icons/hi";
+import { MdLocationOn } from "react-icons/md";
+
+function Header() {
+  const [destination, setDestination] = useState("");
+
+  return (
+    <div className="header">
+      <div className="headerSearch">
+        <div className="headerSearchItem">
+          <MdLocationOn className="headerIcon locationIcon" />
+          <input
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            type="text"
+            placeholder="where to go"
+            className="headerSearchInput"
+            name="destination"
+            id="destination"
+          />
+          <span className="seperator" />
+        </div>
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          <div className="dateDropDown">2023/06/23</div>
+          <span className="seperator" />
+        </div>
+        <div className="headerSearchItem">
+          <div className="optionDropDown">
+            1 adult &bull; 2 chidren &bull; 1 room
+          </div>
+          <span className="seperator" />
+        </div>
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="headerIcon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
