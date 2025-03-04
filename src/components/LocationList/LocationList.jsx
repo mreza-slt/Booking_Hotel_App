cb1c4a9302fb7ab6c3a9c8ecc628b200db5eb687
mreza-slt <rsltany649@gmail.com>
2025-03-04 15:08:17 +0330

    feat(location-list): add LocationList component and useFetch hook
    - Implement LocationList component to display a list of locations
    - Create useFetch custom hook for fetching data from API
    - Integrate useFetch hook into LocationList component
    - Update App.jsx to include the new LocationList component
    
    Created new files:
    - src/components/LocationList/LocationList.jsx
    - src/hooks/useFetch.js

---
+++ src/components/LocationList/LocationList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import useFetch from "../../hooks/useFetch";
+
+function LocationList() {
+  const { data, isLoading } = useFetch("http://localhost:3000/hotels", "");
+
+  if (isLoading) {
+    <p>loading...</p>;
+  }
+
+  return (
+    <div className="nearbyLocation">
+      <h2>Nearby Location</h2>
+      <div className="locationList">
+        {data.map((item) => {
+          return (
+            <div className="locationItem" key={item.id}>
+              <img src={item.picture_url.url} alt={item.name} />
+              <div className="locationItemDisk">
+                <p className="location">{item.smart_location}</p>
+                <p className="name">{item.name}</p>
+                <p className="price">
+                  $&nbsp;{item.price}&nbsp;
+                  <span>night</span>
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default LocationList;
